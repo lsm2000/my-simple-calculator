91c673639587c422659995ae0f3834164a4143c9
lsm2000 <100738479+lsm2000@users.noreply.github.com>
2022-05-04 09:31:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup (
   author = 'lsm',
   author_email ='dytjr6427@naver.com',
   url = "https://github.com/lsm2000/my-simple-calculator",
+   downlaod_url = "https://github.com/lsm2000/my-simple-calculator/releases/tag/v1.0.0",
   install_requires = ['pytest'],
   long_description = 'oss-dev my simple calc',
   long_description_content_type = 'text/markdown',

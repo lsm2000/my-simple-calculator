0c4a5b9c73260351301667838f33bf7882c0fb34
lsm2000 <100738479+lsm2000@users.noreply.github.com>
2022-05-04 09:26:36 +0900

    Create sepup.py

---
+++ sepup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup (
+   include_package_data = True,
+  name = 'mysimplecalc01',
+  version = '0.0.1',
+  description = 'oss-dev mysimplecalc01',
+  author = 'lsm',
+  author_email ='dytjr6427@naver.com',
+  url = "https://github.com/lsm2000/my-simple-calculator",
+  install_requires = ['pytest'],
+  long_description = 'oss-dev my simple calc',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+     "Programming Language :: python :: 3 ",
+    "Operationg System :: OS Independent",
+  ],
+)
+  
+  
